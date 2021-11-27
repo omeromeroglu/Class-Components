e85793aebfb08b888b1ed0d7588568f33158af19
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2021-11-27 19:38:22 +0300

    rewritten with class components

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, Component} from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,6 +9,50 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
+class Users extends Component {
+
+  constructor() {
+    super();
+    this.state= {
+
+      showUsers : true,
+
+    };
+
+  }
+    toggleUsersHandler()  {
+      this.setState((curState) => {
+        
+        return {showUsers: !curState.showUsers}});
+      };
+
+  
+  
+  render() {
+
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+    return (
+      <div className={classes.users}>
+      <button onClick={this.toggleUsersHandler.bind(this)}>
+        {this.state.showUsers ? 'Hide' : 'Show'} Users
+      </button>
+      {this.state.showUsers && usersList}
+    </div>
+    );
+  }
+}
+
+export default Users;
+
+
+
+/* 
 const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
 
@@ -35,3 +79,4 @@ const Users = () => {
 };
 
 export default Users;
+ */
